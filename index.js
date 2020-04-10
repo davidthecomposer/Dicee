7056c571533a0c95b33186f61c59c71e4eecc934
David Campbell <davidthecomposer@gmail.com>
2020-04-10 14:02:28 -0600

    code update

--- index.js
+++ index.js
@@ -1,79 +1,34 @@
-// alert("working!")
+// jshint esversion: 6
 
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const initGame = () => {
 
-let randomDiceImage = `images/dice${randomNumber1}.png`;
-let randomDiceImage2 = `images/dice${randomNumber2}.png`;
+    const playAgain = document.querySelector('.play-again');
 
-let image1 = document.querySelectorAll("img")[0];
-let image2 = document.querySelectorAll("img")[1];
+    const runGame = () => {
 
-image1.setAttribute('src', randomDiceImage);
-image2.setAttribute('src', randomDiceImage2);
-let fireEmoji = '🔥';
-// let winningMessage1 = document.querySelector('h1').textContent = 'Player 1 Wins!';
-// let winningMessage2 = document.querySelector('h1').textContent = 'Player 2 Wins!';
-// let drawMessage = document.querySelector('h1').textContent = 'Draw';
+        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-randomNumber1 > randomNumber2 ? document.querySelector('h1').textContent = fireEmoji + 'Player 1 Wins!' : randomNumber1 === randomNumber2 ? document.querySelector('h1').textContent = 'Draw' : document.querySelector('h1').textContent = 'Player 2 Wins!' + fireEmoji;
+        let randomDiceImage = `images/dice${randomNumber1}.png`;
+        let randomDiceImage2 = `images/dice${randomNumber2}.png`;
+
+        let image1 = document.querySelectorAll("img")[0];
+        let image2 = document.querySelectorAll("img")[1];
+
+        image1.setAttribute('src', randomDiceImage);
+        image2.setAttribute('src', randomDiceImage2);
+        let fireEmoji = '🔥';
 
 
-// let dice1 = document.querySelector(".img1");
-// let dice2 = document.querySelector(".img2");
-//
-//
-//
-//   switch (randomNumber1) {
-//     case 0:
-//       randomNumber1 === 1;
-//       dice1.setAttribute('src', 'images/dice1.png');
-//       break;
-//     case 1:
-//       randomNumber1 === 2;
-//       dice1.setAttribute('src', "images/dice2.png");
-//       break;
-//     case 2:
-//       randomNumber1 === 3;
-//      dice1.setAttribute('src', 'images/dice3.png');
-//       break;
-//     case 3:
-//       randomNumber1 === 4;
-//       dice1.setAttribute('src', "images/dice4.png");
-//       break;
-//     case 4:
-//       randomNumber1 === 5;
-//       dice1.setAttribute('src', "images/dice5.png");
-//       break;
-//     case 5:
-//       randomNumber1 === 6;
-//       dice1.setAttribute('src', "images/dice6.png");
-//       break;
-//   }
-//
-//   switch (randomNumber2) {
-//     case 0:
-//       randomNumber1 === 1;
-//       dice2.setAttribute('src', "images/dice1.png");
-//         break;
-//     case 1:
-//       randomNumber1 === 2;
-//       dice2.setAttribute('src', "images/dice2.png");
-//         break;
-//     case 2:
-//       randomNumber1 === 3;
-//       dice2.setAttribute('src', "images/dice3.png");
-//         break;
-//     case 3:
-//       randomNumber1 === 4;
-//       dice2.setAttribute('src', "images/dice4.png");
-//       break;
-//     case 4:
-//       randomNumber1 === 5;
-//       dice2.setAttribute('src', "images/dice5.png");
-//       break;
-//     case 5:
-//       randomNumber1 === 6;
-//       dice2.setAttribute('src', "images/dice6.png");
-//       break;
-//   }
+        randomNumber1 > randomNumber2 ? document.querySelector('h1').textContent = fireEmoji + 'Player 1 Wins!' : randomNumber1 === randomNumber2 ? document.querySelector('h1').textContent = 'Draw' : document.querySelector('h1').textContent = 'Player 2 Wins!' + fireEmoji;
+
+        playAgain.textContent = 'Play Again';
+
+    };
+
+    playAgain.addEventListener('click', runGame);
+
+}
+
+
+initGame();
